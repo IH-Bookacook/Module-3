6fdcb75e18a43b8af0f4ac723fd129b30f761d4e
Reshma Patel <reshma1529@gmail.com>
2017-10-27 15:01:03 +0200

    before deploy

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,18 +7,52 @@
         <a class="navbar-item" href="/">
           <img src="./assets/img1.jpg">
         </a>
+        <div class="navbar-menu">
+          <div class="navbar-start">
 
-
+            <router-link v-if="!$root.user" to="/" class="navbar-item ">
+              Home
+            </router-link>
+          </div>
+        </div>
         <div class="navbar-burger burger" data-target="navMenuTransparentExample">
-
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </div>
 
       <div id="navMenuTransparentExample" class="navbar-menu">
           <div class="navbar-end">
-          <router-link to="/dashboard" class="navbar-item ">
-            Dashboard
-          </router-link>
+
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <div class="navbar-link">
+
+              Explore
+
+            </div>
+
+            <div id="exploreDropdown" class="navbar-dropdown is-boxed">
+              <!-- <a class="navbar-item " href="BD.vue"> -->
+                <p>
+                  <router-link v-if="!$root.user" to="/bd" class="navbar-item">
+                  <strong>BD</strong>
+                  </router-link>
+
+                  <router-link v-if="!$root.user" to="/Authors" class="navbar-item">
+                  <strong>Author</strong>
+                  </router-link>
+
+                  <router-link v-if="!$root.user" to="/Editors" class="navbar-item">
+                  <strong>Editors</strong>
+                  </router-link>
+                </p>
+              </a>
+
+            </div>
+          </div>
+
           <router-link v-if="!$root.user" to="/login" class="navbar-item ">
             Login
           </router-link>
--- client/src/router/Home.vue
+++ client/src/router/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Home</h1>
+    Hello
     <p v-if="$root.user">Hi {{ $root.user.name }}</p>
   </div>
 </template>
